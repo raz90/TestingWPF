d6b3e80d668f4acec0b3dfc73fbcaf74dd3e39b4
raz90 <vladmir346@yahoo.co.uk>
2015-03-31 16:31:20 +0800

    Add files
    Testing the WPF

---
+++ WpfHello/WpfHello/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WpfHello
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+       
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            string file_name = "test.txt";
+
+            System.IO.StreamWriter objWriter; 
+            objWriter= new System.IO.StreamWriter(file_name);
+            objWriter.Write(textbox1.Text);
+            objWriter.Close();
+
+            
+
+            MessageBox.Show("wrote file");
+
+            
+        }
+
+        private void SaveButton_TextInput(object sender, TextCompositionEventArgs e)
+        {
+
+           
+            
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 windows1;
+
+            windows1 = new Window1();
+
+            windows1.Show();
+        }
+
+    }
+}
---
+++ WpfHello/WpfHello/Window1.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfHello
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            WpfHello.new_schemaDataSet new_schemaDataSet = ((WpfHello.new_schemaDataSet)(this.FindResource("new_schemaDataSet")));
+            // Load data into the table user. You can modify this code as needed.
+            WpfHello.new_schemaDataSetTableAdapters.userTableAdapter new_schemaDataSetuserTableAdapter = new WpfHello.new_schemaDataSetTableAdapters.userTableAdapter();
+            new_schemaDataSetuserTableAdapter.Fill(new_schemaDataSet.user);
+            System.Windows.Data.CollectionViewSource userViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("userViewSource")));
+            userViewSource.View.MoveCurrentToFirst();
+        }
+
+        
+    }
+}
